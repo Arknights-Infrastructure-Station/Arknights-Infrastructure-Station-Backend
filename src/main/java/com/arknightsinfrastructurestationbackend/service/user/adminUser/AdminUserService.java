8f166f93031dc0caafa002fa6a5092c73889dd94
MilkWind <2250957906@qq.com>
2024-12-08 03:07:40 +0800

    refactor(user): 重命名 CommonUserService 为 AdminUserService
    - 将类名从 CommonUserService 修改为 AdminUserService，以更准确地反映其处理管理员用户的功能
    - 更新文件名和类名，以保持代码组织的一致性和清晰性

--- src/main/java/com/arknightsinfrastructurestationbackend/service/user/adminUser/AdminUserService.java
+++ src/main/java/com/arknightsinfrastructurestationbackend/service/user/adminUser/AdminUserService.java
@@ -13,11 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CommonUserService extends BaseCommonUserService<AdminUser> {
+public class AdminUserService extends BaseCommonUserService<AdminUser> {
     private final AdminUserMapper adminUserMapper;
     private final SelectAdminUserService selectAdminUserService;
 
-    public CommonUserService(
+    public AdminUserService(
             AdminUserMapper adminUserMapper,
             SelectAdminUserService selectAdminUserService,
             PasswordEncoder passwordEncoder,
